1d72f8e0a191336fa97709e9aeeef915298a3e50
supermanlan <94032055+supermanlan@users.noreply.github.com>
2025-07-27 15:39:23 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# superlan-music
+# Superlan Music
+
+多人在线同步听歌 + 聊天的网页应用。
---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="app">
+    <h1>🎵 Superlan Music</h1>
+    <p>欢迎来到超级听歌聊天室！</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+}
+</script>
+
+<style>
+body {
+  font-family: sans-serif;
+  text-align: center;
+  background: linear-gradient(to right, #dce35b, #45b649);
+  color: #333;
+}
+</style>
