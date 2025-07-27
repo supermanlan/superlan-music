e57329ce0b9b69f4a72e160d5a1ff4e7a720715e
supermanlan <94032055+supermanlan@users.noreply.github.com>
2025-07-27 15:49:14 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/superlan-music/',
   plugins: [vue()]
 })
